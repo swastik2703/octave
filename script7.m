1dc96a8b9c129ab0424da78cd97c1969b74c88fe
swastik2703 <swastikvasistha2127@gmail.com>
2021-06-03 21:44:30 -0400

    compound interest

---
+++ script7.m
@@ -0,0 +1,10 @@
+﻿m=input('enter the number of students')
+p=input('enter the number of students')
+c=input('enter the number of students')
+m_p=input('enter the students')
+m_c=input('enter no of students')
+c_p=input('enter the no of students')
+all=input('enter total students')
+TS=m+p+c-m_p-m_c-c_p+all
+fprintf('the total students are=%f',TS)
+
