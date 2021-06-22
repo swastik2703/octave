247cb16383e4b482f350b0cf2f49c6242f186b8a
swastik2703 <swastikvasistha2127@gmail.com>
2021-06-21 22:38:04 -0400

    lab-4 dm q1

---
+++ 3.m
@@ -0,0 +1,23 @@
+﻿fprintf("BHAVESH KRISHAN GARG\n");
+
+k=input("Enter 1st number")
+
+l=input("Enter 2nd number")
+
+sum=0;
+
+for (i=k:l);
+
+sum=sum+i
+
+sqrt=i*i
+
+add=sqrt+sum
+
+end
+
+fprintf("Sum = %d\n",sum);
+
+fprintf("Sqrt = %d\n",sqrt);
+
+fprintf("Add = %d\n",add);
