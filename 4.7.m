8a4f63f416d5c3561b7d13655144866a827fa1ed
swastik2703 <swastikvasistha2127@gmail.com>
2021-06-24 22:48:50 -0400

    lab-4 dm q4.7

---
+++ 4.7.m
@@ -0,0 +1,15 @@
+﻿n=input("Enter the number = ");
+if(n>0)
+  if(rem(n,3)==0 || rem(n,5)==0 )
+    n=n*2
+  endif
+end
+if(n<0 || n>-5)
+i=0;
+  m=n;
+  while i<2
+    n=n*m;
+i=i+1;
+endwhile
+  n=n
+end
