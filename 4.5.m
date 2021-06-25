14a1b4060c6985523bf736886b30622ed42e111b
swastik2703 <swastikvasistha2127@gmail.com>
2021-06-24 22:47:57 -0400

    lab-4 dm q4.5

---
+++ 4.5.m
@@ -0,0 +1,9 @@
+﻿p=input("Enter the principle value =");
+r=input("Enter the rate Interest = ");
+n=input("Enter the time =");
+i=1;
+while i<=n
+  ci=(p*((1+(r/100))**i))-p;
+fprintf("%d year CI = %d \n",i,ci)
+i=i+1;
+end
