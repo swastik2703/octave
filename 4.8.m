84b3334c969dcaae9219c2e0d7c127e70872477f
swastik2703 <swastikvasistha2127@gmail.com>
2021-06-25 23:31:41 -0400

    lab-4 dm q4.8

---
+++ 4.8.m
@@ -0,0 +1,19 @@
+﻿A=input ("Enter a matrix A")
+disp(A);
+s=size(A) ;
+B=zeros (s) ;
+i=1;
+while i<=s
+j=1;
+while j<=s
+if (A(i, j) >0)
+B(i, j)=sqrt(A(i, j));
+elseif(A(i, j)<0)
+B(i, j)=B(i, j)+50;
+end
+j=j+1;
+end
+i=i+1;
+end
+disp("matrix B=");
+disp(B) ;
